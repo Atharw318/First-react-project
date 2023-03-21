8ad35d75aeb732d304a583eda2025d83736083b5
atharw <atharw010@gmail.com>
2023-03-21 14:49:24 +0530

    line add and subtract

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
 const [value, setValue] = useState(0)
   return (
     <div className='App'>
+      <h2>
+        ADD & Subtract
+      </h2>
       <div>
       <button className='first1' onClick={() =>setValue(value+1)}> +</button>
       <button className='first' onClick={() =>setValue(value-1)}> -</button>
